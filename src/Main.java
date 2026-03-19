07e8de83a4f259bcc09fd4fd0d5ded5c35f8428d
antonwessel <anton.wessel01@outlook.com>
2026-03-19 01:06:39 +0100

    Add name input

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class Main {
+void main() {
+    IO.println("Hello! My name is Nova.");
+    IO.println("I was created in 2026.");
+    IO.println("Please, remind me your name.");
+    String username = IO.readln();
+    IO.println("What a great name you have, " + username + "!");
 }
