1ebb218a79b898bb2527dcac8825f43f295232da
antonwessel <anton.wessel01@outlook.com>
2026-03-19 02:06:47 +0100

    Add age guess

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ void main() {
     IO.println("Hello! My name is Nova.");
     IO.println("I was created in 2026.");
     IO.println("Please, remind me your name.");
-    String username = IO.readln();
-    IO.println("What a great name you have, " + username + "!");
+    String name = IO.readln();
+    IO.println("What a great name you have, " + name + "!");
+
+    IO.println("Let me guess your age.");
+    IO.println("Enter remainders of dividing your age by 3, 5 and 7.");
+    int remainder3 = Integer.parseInt(IO.readln());
+    int remainder5 = Integer.parseInt(IO.readln());
+    int remainder7 = Integer.parseInt(IO.readln());
+    int age = (remainder3 * REMAINDER_3_MULTIPLIER + remainder5 * REMAINDER_5_MULTIPLIER +
+            remainder7 * REMAINDER_7_MULTIPLIER) % AGE_MODULO;
+    IO.println("Your age is " + age + "; that's a good time to start programming!");
 }
+
+public static final int AGE_MODULO = 105;
+public static final int REMAINDER_3_MULTIPLIER = 70;
+public static final int REMAINDER_5_MULTIPLIER = 21;
+public static final int REMAINDER_7_MULTIPLIER = 15;
