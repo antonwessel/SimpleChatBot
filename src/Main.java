6520f61400afe9df4719ca678833c91e48f01233
antonwessel <anton.wessel01@outlook.com>
2026-03-26 21:06:54 +0100

    Improve chatbot input validation

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,9 @@ private static void remindName() {
 private static void guessAge() {
     IO.println("Let me guess your age.");
     IO.println("Enter remainders of dividing your age by 3, 5 and 7.");
-    int remainder3 = Integer.parseInt(IO.readln());
-    int remainder5 = Integer.parseInt(IO.readln());
-    int remainder7 = Integer.parseInt(IO.readln());
+    int remainder3 = readRemainder(3);
+    int remainder5 = readRemainder(5);
+    int remainder7 = readRemainder(7);
     int age = (remainder3 * REMAINDER_3_MULTIPLIER + remainder5 * REMAINDER_5_MULTIPLIER +
             remainder7 * REMAINDER_7_MULTIPLIER) % AGE_MODULO;
     IO.println("Your age is " + age + "; that's a good time to start programming!");
@@ -31,7 +31,11 @@ private static void guessAge() {
 
 private static void count() {
     IO.println("Now I will prove to you that I can count to any number you want.");
-    int number = Integer.parseInt(IO.readln());
+    int number = readInt();
+    while (number < 0) {
+        IO.println("Please, enter a non-negative number.");
+        number = readInt();
+    }
     for (int i = 0; i <= number; i++) {
         IO.println(i + "!");
     }
@@ -45,15 +49,35 @@ private static void test() {
     IO.println("2. To decompose a program into several subroutines.");
     IO.println("3. To determine the execution time of a program.");
     IO.println("4. To interrupt the execution of a program.");
-    int answer = Integer.parseInt(IO.readln());
+    int answer = readInt();
     while (answer != correctAnswer) {
         IO.println("Please, try again.");
-        answer = Integer.parseInt(IO.readln());
+        answer = readInt();
     }
 }
 
 private static void congratulate() {
     IO.println("Congratulations, have a nice day!");
+}
+
+private static int readInt() {
+    while (true) {
+        try {
+            return Integer.parseInt(IO.readln());
+        } catch (NumberFormatException e) {
+            IO.println("Please, enter a number.");
+        }
+    }
+}
+
+private static int readRemainder(int divisor) {
+    while (true) {
+        int remainder = readInt();
+        if (remainder >= 0 && remainder < divisor) {
+            return remainder;
+        }
+        IO.println("Please, enter a valid remainder.");
+    }
 }
 
 private static final int AGE_MODULO = 105;
